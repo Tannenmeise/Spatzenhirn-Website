baca4ad7463f3d353439523bd0ebbf739d04f9b7
Tamara Hezel <tamara.hezel@hs-furtwangen.de>
2020-09-03 16:00:25 +0200

    designed header + footer

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+document.getElementById("arrow")?.addEventListener("click", handleArrowClick);
+document.getElementById("arrowUp")?.addEventListener("click", handleArrowClick);
+function handleArrowClick(_event) {
+    console.log("Called Event Listener");
+    let arrow = _event.target;
+    if (arrow.id == "arrow") {
+        document.getElementById("collapseable").setAttribute("style", "visibility: visible");
+        document.getElementById("arrow").setAttribute("src", "graphics/buttons/arrowUp.png");
+        document.getElementById("arrow").setAttribute("id", "arrowUp");
+    }
+    else {
+        console.log("Called else statement");
+        document.getElementById("collapseable").setAttribute("style", "visibility: hidden");
+        document.getElementById("arrowUp").setAttribute("src", "graphics/buttons/arrow.png");
+        document.getElementById("arrowUp").setAttribute("id", "arrow");
+    }
+}
+//# sourceMappingURL=script.js.map
